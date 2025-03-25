06c784bf5e6f9640dd84e392b31111de9d994213
Gusanidas <Alejandrogus792@gmail.com>
2025-03-25 01:37:43 +0100

    some change6

--- trl/trainer/grpo_trainer.py
+++ trl/trainer/grpo_trainer.py
@@ -797,6 +797,7 @@ class GRPOTrainer(Trainer):
         self, inputs: dict[str, Union[torch.Tensor, Any]]
     ) -> dict[str, Union[torch.Tensor, Any]]:
         device = self.accelerator.device
+        prompts = [x["prompt"] for x in inputs]
         prompts_text = [maybe_apply_chat_template(example, self.processing_class)["prompt"] for example in inputs]
         prompt_inputs = self.processing_class(
             text=prompts_text, return_tensors="pt", padding=True, padding_side="left", add_special_tokens=False
